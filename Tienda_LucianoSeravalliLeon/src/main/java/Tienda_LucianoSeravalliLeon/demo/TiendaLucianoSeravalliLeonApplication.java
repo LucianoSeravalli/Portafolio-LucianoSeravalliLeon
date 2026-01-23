3ee178a1c13fd099bee837f606fe9bc8b4053e48
LucianoSeravalli <seravalliluciano@gmail.com>
2026-01-22 23:25:32 -0600

    Semana 2

---
+++ Tienda_LucianoSeravalliLeon/src/main/java/Tienda_LucianoSeravalliLeon/demo/TiendaLucianoSeravalliLeonApplication.java
@@ -0,0 +1,13 @@
+package Tienda_LucianoSeravalliLeon.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TiendaLucianoSeravalliLeonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TiendaLucianoSeravalliLeonApplication.class, args);
+	}
+
+}
---
+++ Tienda_LucianoSeravalliLeon/src/test/java/Tienda_LucianoSeravalliLeon/demo/TiendaLucianoSeravalliLeonApplicationTests.java
@@ -0,0 +1,13 @@
+package Tienda_LucianoSeravalliLeon.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TiendaLucianoSeravalliLeonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
