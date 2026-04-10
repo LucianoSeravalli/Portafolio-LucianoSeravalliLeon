5d792ab93efca520e5835499a0e3bbc45f4f8017
LucianoSeravalli <seravalliluciano@gmail.com>
2026-04-09 18:49:06 -0600

    Semana 12

---
+++ Tienda_LucianoSeravalliLeon/src/main/java/Tienda_LucianoSeravalliLeon/repository/VentaRepository.java
@@ -0,0 +1,12 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package Tienda_LucianoSeravalliLeon.repository;
+
+import Tienda_LucianoSeravalliLeon.domain.Venta;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface VentaRepository extends JpaRepository<Venta, Integer>{
+    
+}
